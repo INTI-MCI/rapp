8b9a2f7dfb73522b9def3fae3fb2028e39bf23e3
Admin <Admin@ptmwsf2243.inti.local>
2023-10-02 16:27:01 -0300

    Add new parameters to simulate function

--- cli.py
+++ cli.py
@@ -13,22 +13,30 @@ def main():
     PHASE = -np.pi/4
 
     plot = Plot(ylabel="Voltage [V]", xlabel="Time [s]")
-    plot.add_data(*simulate(t=2), style='-', color='k', label='φ=0')
+    plot.add_data(*simulate(t=1), style='-', color='k', label='φ=0')
     plot.add_data(
-        *simulate(t=2, phi=PHASE), style='--', color='k', label=f'φ={round(PHASE, 2)}')
+        *simulate(t=1, phi=PHASE), style='--', color='k', label=f'φ={round(PHASE, 2)}')
 
     plot.save(title='Two signals without noise')
     plot.show()
 
     plot.clear()
 
-    plot.add_data(*simulate(t=2, awgn=0.1), style='-', color='k', label='φ=0')
+    plot.add_data(*simulate(t=1, awgn=0.1), style='-', color='k', label='φ=0')
     plot.add_data(
-        *simulate(t=2, phi=PHASE, awgn=0.1), style='--', color='k', label=f'φ={round(PHASE, 2)}'
+        *simulate(t=1, phi=PHASE, awgn=0.1), style='--', color='k', label=f'φ={round(PHASE, 2)}'
     )
 
     plot.save(title='Two signals with AWGN noise')
     plot.show()
+
+    print(f"True phase diff: {np.rad2deg(PHASE)}")
+    ts, s1 = simulate(t=5, phi=0)
+    ts, s2 = simulate(t=5, phi=PHASE)
+
+    phase_diff = np.arccos(np.dot(s1, s2) / (np.linalg.norm(s1) * np.linalg.norm(s2)))
+    error = PHASE + phase_diff
+    print(f"Phase diff (cosine similarity): {np.rad2deg(phase_diff)}. Error: {np.rad2deg(error)}")
 
 
 if __name__ == '__main__':
