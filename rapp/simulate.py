b30a88599b62cb39f3ad32f325d2799438524a4f
Tomás Link <tomaslink@gmail.com>
2023-10-10 02:28:20 -0300

    Update simulation parameters

--- rapp/simulate.py
+++ rapp/simulate.py
@@ -1,32 +1,27 @@
 import numpy as np
 
 
-def simulate(
-    A: float = 4,
-    sf: int = 57600,
-    f: int = 50,
-    t: int = 1,
+def harmonic_signal(
+    A: float = 5,
+    N: int = 1,
+    fs: int = 50,
     phi: float = 0,
-    bits: int = 10,
     awgn: float = None
 ) -> tuple:
     """Simulates a harmonic signal.
 
     Args:
         A: amplitude (peak) of the signal.
-        sf: sampling frequency (Hz).
-        f: frequency of the signal (Hz).
-        t: number of seconds to simulate.
+        N: number of cycles.
+        fs: sampling frequency (samples per cycle).
         phi: phase (radians).
-        bits: number of bits of the signal.
         awgn: amount of additive white gaussian noise, relative to A.
 
     Returns:
         The signal as an (x, y) tuple.
     """
 
-    samples_per_second = int(sf / bits)
-    x = np.linspace(0, 2 * np.pi * f * t, num=samples_per_second * t)
+    x = np.linspace(0, 2 * np.pi * N, num=N * fs)
 
     signal = A * np.sin(x + phi)
 
