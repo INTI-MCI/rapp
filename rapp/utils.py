fb6391ce6e4b70b51ec2c1b1a2a3ee6e63eab9d2
Tomás Link <tomaslink@gmail.com>
2023-10-10 02:28:20 -0300

    Update simulation parameters

--- rapp/utils.py
+++ rapp/utils.py
@@ -1,9 +1,15 @@
 import os
 import shutil
 
+import math
+
 
 def create_folder(folder, overwrite=True):
     if os.path.exists(folder) and overwrite:
         shutil.rmtree(folder)
 
     os.makedirs(folder, exist_ok=True)
+
+
+def round_to_n(number, n):
+    return round(number, n - int(math.floor(math.log10(abs(number)))) - 1)
