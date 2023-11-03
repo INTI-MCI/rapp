17332e4c04395277ccf3935ff42fee98c243f3bf
Tomás Link <tomaslink@gmail.com>
2023-11-02 23:21:33 -0300

    Add nice CLI interface

--- rapp/polarimeter.py
+++ rapp/polarimeter.py
@@ -1,53 +1,30 @@
 import os
-import sys
 import time
+import logging
+
 
 from datetime import datetime
 
-from esp import ESP  # noqa
 import serial        # noqa
+from rapp.esp import ESP  # noqa
+from rapp.mocks import SerialMock, ESPMock
 
+
+logger = logging.getLogger(__name__)
 
 FILE_HEADER = "ANGLE [°], A0 [V], A1 [V], DATETIME"
 FILE_ROW = "{angle} {a0} {a1} {datetime}"
 
-OUTPUT_DIR = "mediciones"
+OUTPUT_DIR = "output"
 
 ADC_DEVICE = 'COM4'
 ADC_BAUDRATE = 57600
 ADC_TIMEOUT = 0.1
 ADC_MAX_VAL = 4.096
 
-
-class SerialMock:
-    """Mock object for serial.Serial."""
-    def readline(self):
-        return b'3.4567,2.3422'
-
-    def close(self):
-        pass
-
-    def flushInput(self):
-        pass
-
-
-class ESPMock:
-    """Mock object for esp.ESP."""
-    dev = SerialMock()
-
-    pos = 0
-    vel = 2
-
-    def setpos(self, pos, axis=None):
-        self.pos = pos
-        return pos
-
-    def getpos(self, axis=None):
-        return self.pos
-
-    def setvel(self, vel, axis=None):
-        self.vel = vel
-        return vel
+ANALYZER_DEVICE = "COM3"
+ANALYZER_BAUDRATE = 921600
+ANALYZER_AXIS = 1
 
 
 def create_file(filename):
@@ -64,6 +41,17 @@ def create_or_open_file(filename, overwrite):
         file = open(filename, 'a')
 
     return file
+
+
+def ask_for_overwrite(filename):
+    overwrite = False
+    if os.path.exists(filename):
+        i = input(
+            "File already exists. Do you want to erase it? y/n (default is NO): ")
+        if i == 's':
+            overwrite = True
+
+    return overwrite
 
 
 def parse_data(data):
@@ -85,79 +73,65 @@ def parse_data(data):
     return a0, a1
 
 
-def main(n_cycles=1, step=10, n_points=10, delay=0, filename='test', verbose=False):
+def frange(start, end, step):
+    """A range with float step allowed."""
+    return [p * step for p in range(start, int(end / step))]
 
-    angles = [i for i in range(0, 360 * n_cycles, step)]
-    filename = "{}-n_cycles{}-step{}-n_points{}.txt".format(filename, n_cycles, step, n_points)
 
+def main(
+    cycles=1, step=10, samples=10, delay_position=1, delay_angle=0, analyzer_velocity=2,
+    filename='test', test=False
+):
+
+    filename = "{}-cycles{}-step{}-samples{}.txt".format(filename, cycles, step, samples)
     os.makedirs(OUTPUT_DIR, exist_ok=True)
-    filename = os.path.join(OUTPUT_DIR, filename)
+    filepath = os.path.join(OUTPUT_DIR, filename)
+    overwrite = ask_for_overwrite(filepath)
 
-    # Mock objects to test when we don't have the device connected.
-    # TODO: create a unit test and use them from there.
-    serialport = SerialMock()
-    analyzer = ESPMock()
+    angles = [i for i in frange(0, 360 * cycles, step)]
 
-    # serialport = serial.Serial(ADC_DEVICE, ADC_BAUDRATE, timeout=ADC_TIMEOUT)
-    # analyzer = ESP(dev="COM3", b=921600, axis=1, reset=True)
+    logger.debug("Angles to measure: {}".format(angles))
 
-    time.sleep(2)           # Why this?
-    analyzer.setvel(vel=4)
+    if test:
+        # If this happens, we don't use real connections. We use mock objects to test the code.
+        # TODO: create a unit test and use the mocks from there.
+        serialport = SerialMock()
+        analyzer = ESPMock()
+    else:
+        serialport = serial.Serial(ADC_DEVICE, ADC_BAUDRATE, timeout=ADC_TIMEOUT)
+        analyzer = ESP(dev=ANALYZER_DEVICE, b=ANALYZER_BAUDRATE, axis=ANALYZER_AXIS, reset=True)
 
-    overwrite = False
-    if os.path.exists(filename):
-        i = input(
-            "El archivo ya existe. Querés borrar el archivo? s/n (ENTER es NO): ")
-        if i == 's':
-            overwrite = True
+    analyzer.setvel(vel=analyzer_velocity)
 
-    file = create_or_open_file(filename, overwrite)
-
+    file = create_or_open_file(filepath, overwrite)
     for angle in angles:
-        analyzer.setpos(angle)
-
-        time.sleep(1)  # wait for position to stabilize
-        # TODO: assert that the new position is the expected, up to N decimal positions?
-        print(f"Measuring at position: {analyzer.getpos()}")
-
+        analyzer.setpos(angle)   # TODO: Try to obtain the exact position desired instead of .0001.
+        time.sleep(delay_position)  # wait for position to stabilize
         serialport.flushInput()  # Clear buffer. Otherwise messes up measurements at the beginning.
 
+        logger.info(f"Measuring at position: {analyzer.getpos()}")
+
         i = 0
-        while i < n_points:
+        while i < samples:
             data = serialport.readline().decode().strip()
 
             try:
                 a0, a1 = parse_data(data)
             except ValueError as e:
-                print("Found error in data: {}. data: {}. Skipping...".format(e, data))
+                logger.warning("Found error in data: {}. data: {}. Skipping...".format(e, data))
                 continue
 
             datetime_ = datetime.now().isoformat()
             row = FILE_ROW.format(angle=angle, a0=a0, a1=a1, datetime=datetime_)
 
-            if verbose:
-                print(row)
+            logger.debug(row)
 
             file.write(row)
             file.write('\n')
             i = i + 1
 
-            time.sleep(delay)
+            time.sleep(delay_angle)
 
     file.close()
     serialport.close()
     analyzer.dev.close()
-
-
-if __name__ == '__main__':
-    n_cycles = int(sys.argv[1])          # how many cycles.
-    step = int(sys.argv[2])              # every how many degrees to take a measurement.
-    n_points = int(sys.argv[3])          # how many points per angle.
-    delay = float(sys.argv[4])           # the delay (in seconds) between each measurement.
-    filename = str(sys.argv[5])          # the filename.
-    verbose = bool(int(sys.argv[6]))     # whether to print each measurement. 1 (true) - 0 (false).
-
-    # example:
-    # python polarimeter.py 1 180 10 0 test.txt 0
-
-    main(n_cycles, step, n_points, delay, filename, verbose)
