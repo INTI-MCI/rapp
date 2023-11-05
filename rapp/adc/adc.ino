9cd4ebe0a5588025f1ecc08c6e97ac50f7d13947
Tomás Link <tomaslink@gmail.com>
2023-11-05 03:37:29 -0300

    Make arduino wait for a signal with the no. of samples to measure.

--- rapp/adc/adc.ino
+++ rapp/adc/adc.ino
@@ -2,13 +2,12 @@
 #include <Adafruit_ADS1X15.h>
 
 Adafruit_ADS1115 ads;
-const float multiplier = 0.125F;
-const unsigned short int BAUDRATE = 57600;
-const bool ADC_READING_MODE_CONTINUOUS = true;
-const unsigned long CONTINUOUS_MODE_DELAY_MUS = 580; // This gives ~855 SPS.
+const unsigned short int SERIAL_BAUDRATE = 57600;
+const bool ADS_READING_MODE_CONTINUOUS = true;
+const unsigned long ADS_CONTINUOUS_MODE_DELAY_MUS = 580; // This gives ~855 SPS.
 
 void setup(void) {
-    Serial.begin(BAUDRATE);
+    Serial.begin(SERIAL_BAUDRATE);
 
     // Change scale factor:
     // ads.setGain(GAIN_TWOTHIRDS);  // +/- 6.144V  1 bit = 0.1875mV (default)
@@ -19,16 +18,17 @@ void setup(void) {
     // ads.setGain(GAIN_SIXTEEN);    // +/- 0.256V  1 bit = 0.0078125mV
 
     ads.setDataRate(RATE_ADS1115_860SPS);
-    ads.begin(); 
+    ads.begin();
 }
 
 void measure_sps() {
+    // This function was useful to measure the samples per second
     float starttime;
     float endtime;
     starttime = millis();
     endtime = starttime;
 
-    ads.startADCReading(MUX_BY_CHANNEL[0], ADC_READING_MODE_CONTINUOUS);
+    ads.startADCReading(MUX_BY_CHANNEL[0], ADS_READING_MODE_CONTINUOUS);
 
     long N = 0;
     while ((endtime - starttime) <= 1000)
@@ -36,23 +36,29 @@ void measure_sps() {
         short value0_bits = ads.getLastConversionResults();
         N = N + 1;
         endtime = millis();
-        delayMicroseconds(CONTINUOUS_MODE_DELAY_MUS);
+        delayMicroseconds(ADS_CONTINUOUS_MODE_DELAY_MUS);
     }
-    Serial.print("MUESTRAS POR SEGUNDO:");
+    Serial.print("SAMPLES PER SECOND: ");
     Serial.println(N);
 }
+
 void loop(void) {
-    measure_sps();
 
-    //  float value0 = bits2volts(value0_bits);
-    //  float value1 = bits2volts(value1_bits);
+    if (Serial.available() > 0) {  // Wait to recieve a signal.
 
-    //  Serial.print(value0, 6);
-    //  Serial.print(',');
-    //  Serial.println(value1, 6);
-}
+        ads.startADCReading(MUX_BY_CHANNEL[0], ADS_READING_MODE_CONTINUOUS);
+        short samples = Serial.parseInt();
 
+        short i = 0;
+        while (i < samples) {
+            short value0_bits = ads.getLastConversionResults();
+            float value0_volts = ads.computeVolts(value0_bits);
+            
+            Serial.println(value0_volts, 6);
+            i = i + 1;
+            delayMicroseconds(ADS_CONTINUOUS_MODE_DELAY_MUS);
+        };
 
-float bits2volts(float x) {
-  return x * multiplier / 1000.0F;
+        Serial.flush();  // waits for the output data to be transmitted before continue.
+    }
 }
