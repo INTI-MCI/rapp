8c5313a15ebc3abe115b7b13ee51cf56943b6670
Tomás Link <tomaslink@gmail.com>
2023-11-05 16:42:37 -0300

    Add timing decorator to measure functions execution time

--- examples/test_adc.py
+++ examples/test_adc.py
@@ -1,37 +1,50 @@
 import time
 import serial
 
+from rapp.utils import timing
+
+
 # ADC_DEVICE = 'COM4'
 ADC_DEVICE = '/dev/ttyACM0'
 
 ADC_BAUDRATE = 57600
 ADC_TIMEOUT = 0.1
 
-N_SAMPLES = 8
+WAIT_TIME_AFTER_CONNECTION = 2
 
 
-adc = serial.Serial(ADC_DEVICE, ADC_BAUDRATE, timeout=ADC_TIMEOUT)
+@timing
+def acquire(adc, n_samples):
+    adc.write(bytes(str(n_samples), 'utf-8'))
+    # Sending directly the numerical value didn't work.
+    # See: https://stackoverflow.com/questions/69317581/sending-serial-data-to-arduino-works-in-serial-monitor-but-not-in-python  # noqa
 
-print("Esperando segundos...")
-# Arduino resets when a new serial connection is made.
-# We need to wait, otherwise we don't recieve anything.
-# Less than 2 seconds does not work.
-# TODO: check if we can avoid that arduino resets.
-time.sleep(2)
+    data = []
+    while len(data) < n_samples:
+        try:
+            value = adc.readline().decode().strip()
+            if value:
+                print("A0: {}".format(value))
+                data.append(value)
+        except (ValueError, UnicodeDecodeError) as e:
+            print(e)
 
-adc.write(bytes(str(N_SAMPLES), 'utf-8'))
-# Sending directly the numerical value didn't work.
-# See: https://stackoverflow.com/questions/69317581/sending-serial-data-to-arduino-works-in-serial-monitor-but-not-in-python  # noqa
 
-print("Adquiriendo...")
-data = []
-while len(data) < N_SAMPLES:
-    try:
-        value = adc.readline().decode().strip()
-        if value:
-            print("A0: {}".format(value))
-            data.append(value)
-    except (ValueError, UnicodeDecodeError) as e:
-        print(e)
+def main():
+    adc = serial.Serial(ADC_DEVICE, ADC_BAUDRATE, timeout=ADC_TIMEOUT)
 
-adc.close()
+    print("Waiting {} seconds...".format(WAIT_TIME_AFTER_CONNECTION))
+    # Arduino resets when a new serial connection is made.
+    # We need to wait, otherwise we don't recieve anything.
+    # Less than 2 seconds does not work.
+    # TODO: check if we can avoid that arduino resets.
+    time.sleep(WAIT_TIME_AFTER_CONNECTION)
+
+    print("Adquiriendo...")
+    acquire(adc, n_samples=855)
+
+    adc.close()
+
+
+if __name__ == '__main__':
+    main()
