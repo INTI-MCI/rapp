b607c8cae247e1740faef942e629a2aa14b967d0
Tomás Link <tomaslink@gmail.com>
2023-11-08 15:30:05 -0300

    Add CLI command to compute phase difference given a file with data

--- rapp/signal/phase.py
+++ rapp/signal/phase.py
@@ -1,10 +1,17 @@
+import logging
 from dataclasses import dataclass
 
 import numpy as np
 
 from scipy.optimize import curve_fit
 
+from rapp.utils import round_to_n
+
+
 PHASE_DIFFERENCE_METHODS = ['cossim', 'fit']
+
+
+logger = logging.getLogger(__name__)
 
 
 def sine(x, a, phi, c):
@@ -23,24 +30,28 @@ def cosine_similarity(s1, s2):
     return np.arccos(np.dot(s1, s2) / (np.linalg.norm(s1) * np.linalg.norm(s2)))
 
 
-def phase_difference(xs, y1, y2, method='cossim') -> PhaseDifferenceResult:
+def phase_difference(xs, s1, s2, method='cossim') -> PhaseDifferenceResult:
     """Computes phase difference between two harmonic signals (xs, s1) and (xs, s2)."""
 
     if method not in PHASE_DIFFERENCE_METHODS:
         raise ValueError("Phase difference method: {} not implemented.".format(method))
 
     if method == 'cossim':
-        phase_diff = cosine_similarity(y1, y2)
+        phase_diff = cosine_similarity(s1, s2)
         return PhaseDifferenceResult(phase_diff)
 
     if method == 'fit':
-        popt1, pcov1 = curve_fit(sine, xs, y1)
-        popt2, pcov2 = curve_fit(sine, xs, y2)
+        popt1, pcov1 = curve_fit(sine, xs, s1)
+        popt2, pcov2 = curve_fit(sine, xs, s2)
 
         phi1 = popt1[1] % (np.pi)
         phi2 = popt2[1] % (np.pi)
 
-        phase_diff = (phi1 - phi2) % (np.pi)
+        phase_diff = abs(phi1 - phi2)
+
+        logger.debug("φ1 = {}".format(round_to_n(phi1, 3)))
+        logger.debug("φ2 = {}".format(round_to_n(phi2, 3)))
+        logger.debug("|φ1 - φ2| = {}".format(round_to_n(phase_diff, 3)))
 
         fitx = np.arange(min(xs), max(xs), step=0.01)
 
