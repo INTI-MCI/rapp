06b49829f5bfe53f54f7e02f698502c2f17cc4cd
Tomás Link <tomaslink@gmail.com>
2023-11-16 19:43:12 -0300

    Refactor adc.ino to measure properly elapsed time

--- examples/test_adc.py
+++ examples/test_adc.py
@@ -3,10 +3,9 @@ import time
 import serial
 
 from rapp.utils import timing
-from rapp.polarimeter import read_data
 
 # ADC_DEVICE = 'COM4'
-ADC_DEVICE = '/dev/ttyACM1'
+ADC_DEVICE = '/dev/ttyACM0'
 
 ADC_BAUDRATE = 57600
 ADC_TIMEOUT = 0.1
@@ -19,7 +18,25 @@ def bits_to_volts(value):
     return value * ADC_MULTIPLIER_mV / 1000
 
 
-@timing
+def read_data(adc, n_samples, in_bytes=True):
+    data = []
+    for _ in range(n_samples):
+        try:
+            if in_bytes:
+                value = adc.read(2)
+                value = int.from_bytes(value, byteorder='big', signed=True)
+            else:
+                value = adc.readline().decode().strip()
+                value = int(value)
+
+            value = bits_to_volts(value)
+            data.append(value)
+        except (ValueError, UnicodeDecodeError) as e:
+            print(e)
+
+    return data
+
+
 def acquire(adc, n_samples, **kwargs):
     adc.write(bytes(str(n_samples), 'utf-8'))
     # Sending directly the numerical value didn't work.
@@ -35,17 +52,19 @@ def acquire(adc, n_samples, **kwargs):
 
 def main(n_samples=5):
     adc = serial.Serial(ADC_DEVICE, ADC_BAUDRATE, timeout=ADC_TIMEOUT)
-    adc.flushInput()
 
-    print("Waiting {} seconds...".format(WAIT_TIME_AFTER_CONNECTION))
+    print("Waiting {} seconds after opening the connection...".format(WAIT_TIME_AFTER_CONNECTION))
     # Arduino resets when a new serial connection is made.
     # We need to wait, otherwise we don't recieve anything.
     # Less than 2 seconds does not work.
     # TODO: check if we can avoid that arduino resets.
     time.sleep(WAIT_TIME_AFTER_CONNECTION)
 
+    print("Flushing input...")
+    adc.flushInput()
+
     print("Acquiring...")
-    data, elapsed_time = acquire(adc, n_samples=n_samples, in_bytes=True)
+    data, elapsed_time = timing(acquire)(adc, n_samples=n_samples, in_bytes=False)
 
     for d in data:
         print("(A0, A1) = {}".format(d))
--- rapp/adc/adc.ino
+++ rapp/adc/adc.ino
@@ -4,18 +4,17 @@
 Adafruit_ADS1115 ads;
 const unsigned short int SERIAL_BAUDRATE = 57600;
 const bool ADS_READING_MODE_CONTINUOUS = true;
-const byte ADS_READING_DELAY = 5; // Time we need to wait so ADC goes out of suspension
-const bool SEND_DATA_AS_STRING = false;
+const byte ADS_READING_DELAY = 3; // Time we need to wait so ADC goes out of suspension
 
 // 600 gives ~812 SPS when using serial_write_short(), 580 gives ~855 SPS when using println().
-const unsigned long ADS_CONTINUOUS_MODE_DELAY_MUS = 600;
+const unsigned long ADS_CONTINUOUS_MODE_DELAY_MUS = 580;
 
 void setup(void) {
     Serial.begin(SERIAL_BAUDRATE);
 
     // Change scale factor:
-    ads.setGain(GAIN_TWOTHIRDS);  // +/- 6.144V  1 bit = 0.1875mV (default)
-    // ads.setGain(GAIN_ONE);        // +/- 4.096V  1 bit = 0.125mV
+    // ads.setGain(GAIN_TWOTHIRDS);  // +/- 6.144V  1 bit = 0.1875mV (default)
+    ads.setGain(GAIN_ONE);        // +/- 4.096V  1 bit = 0.125mV
     // ads.setGain(GAIN_TWO);        // +/- 2.048V  1 bit = 0.0625mV
     // ads.setGain(GAIN_FOUR);       // +/- 1.024V  1 bit = 0.03125mV
     // ads.setGain(GAIN_EIGHT);      // +/- 0.512V  1 bit = 0.015625mV
@@ -23,27 +22,6 @@ void setup(void) {
 
     ads.setDataRate(RATE_ADS1115_860SPS);
     ads.begin();
-}
-
-void measure_sps() {
-    // This function was useful to measure the samples per second
-    float starttime;
-    float endtime;
-    starttime = millis();
-    endtime = starttime;
-
-    ads.startADCReading(MUX_BY_CHANNEL[0], ADS_READING_MODE_CONTINUOUS);
-
-    long N = 0;
-    while ((endtime - starttime) <= 1000)
-    {
-        short value0_bits = ads.getLastConversionResults();
-        N = N + 1;
-        endtime = millis();
-        delayMicroseconds(ADS_CONTINUOUS_MODE_DELAY_MUS);
-    }
-    Serial.print("SAMPLES PER SECOND: ");
-    Serial.println(N);
 }
 
 void serial_write_short(short data){
@@ -60,20 +38,45 @@ void read_n_samples_from_channel(short n_samples, byte channel){
     short i = 0;
     while (i < n_samples) {
         short data = ads.getLastConversionResults();
-        if (SEND_DATA_AS_STRING)
-          Serial.println(data, DEC);
-        else
-          serial_write_short(data);
+        Serial.println(data, DEC);
+ 
         i = i + 1;
         delayMicroseconds(ADS_CONTINUOUS_MODE_DELAY_MUS);
     };
 }
 
+short acquire(){
+    short n_samples = Serial.parseInt();
+
+    read_n_samples_from_channel(n_samples, 0);      
+    read_n_samples_from_channel(n_samples, 1);
+
+    return n_samples;
+}
+
+void measure_SPS(){
+    float starttime = millis();
+    short n_samples = acquire();
+    float endtime = millis();
+
+    float elapsedtime = (endtime - starttime) / 1000;
+    short sps = (n_samples / elapsedtime);
+
+    Serial.print("SAMPLES: ");
+    Serial.println(n_samples, DEC);
+
+    Serial.print("SECONDS: ");
+    Serial.println(elapsedtime, DEC);
+
+    Serial.print("SAMPLES PER SECOND: ");
+    Serial.println(sps);
+}
+
 void loop(void) {
     if (Serial.available() > 0) {  // Wait to recieve a signal.
-        short n_samples = Serial.parseInt();
-        
-        read_n_samples_from_channel(n_samples, 0);      
-        read_n_samples_from_channel(n_samples, 1);
+
+        measure_SPS();
+
+        //acquire()
     }
 }
