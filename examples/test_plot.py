44a31863c3c4551ea7b283d7d5688b0b13009349
Tomás Link <tomaslink@gmail.com>
2023-12-13 17:28:50 -0300

    Add new measurements

--- examples/test_plot.py
+++ examples/test_plot.py
@@ -1,22 +1,30 @@
+import sys
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
 import rapp.constants as ct
 
-filepath = 'workdir/output-data/2023-12-07-HeNe-noise-cycles0-step10-samples100000.txt'
-data = pd.read_csv(
-    filepath,
-    sep=r"\s+", skip_blank_lines=True, comment='#', header=0, usecols=(0, 1, 2),
-    encoding=ct.ENCONDIG
-)
+
+def main(filepath):
+    data = pd.read_csv(
+        filepath,
+        sep=r"\s+", skip_blank_lines=True, comment='#', header=0, usecols=(0, 1, 2),
+        encoding=ct.ENCONDIG
+    )
+
+    data_ch0 = data['CH0']
+    data_ch1 = data['CH1']
+
+    f, (ax0, ax1) = plt.subplots(1, 2, figsize=(8, 4), sharey=False)
+
+    ax0.plot(data_ch0, markevery=10)
+    ax1.plot(data_ch1, markevery=10)
+
+    plt.show()
 
 
-data_ch0 = data['CH0']
-data_ch1 = data['CH1']
+if __name__ == '__main__':
+    filename = sys.argv[1]
 
-f, (ax0, ax1) = plt.subplots(1, 2, figsize=(8, 4), sharey=False)
-
-ax0.plot(data_ch0)
-ax1.plot(data_ch1)
-
-plt.show()
+    main(filename)
