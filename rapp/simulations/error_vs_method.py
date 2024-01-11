f2d998afbdd01532dfb146671f91ab881c8e4d4f
Tomás Link <tomaslink@gmail.com>
2024-01-11 15:00:20 -0300

    Fix error_vs_method simulation

--- rapp/simulations/error_vs_method.py
+++ rapp/simulations/error_vs_method.py
@@ -15,7 +15,7 @@ METHODS = {  # (marker_style, line_style, reps)
     'ODR': ('-', 'dotted', None)
 }
 
-TPL_LOG = "cycles={}, time={} m, φerr: {}."
+TPL_LOG = "cycles={}, φerr: {}."
 TPL_LABEL = "samples={}\nstep={}°"
 TPL_FILENAME = "sim_error_vs_method-reps-{}-samples-{}-step-{}.png"
 
@@ -33,12 +33,12 @@ def run(phi, folder, samples=5, step=1, reps=10, max_cycles=8, show=False):
 
         fc = simulator.samples_per_cycle(step=step)
 
-        logger.info("Method: {}, fc={}, reps={}".format(method, fc, reps))
+        logger.info("Method: {}, reps={}".format(method, mreps))
 
         method_errors = []
         for cycles in cycles_list:
             n_res = simulator.n_simulations(
-                n=reps,
+                n=mreps,
                 method=method,
                 cycles=cycles,
                 fc=fc,
@@ -47,7 +47,8 @@ def run(phi, folder, samples=5, step=1, reps=10, max_cycles=8, show=False):
                 a0_noise=simulator.A0_NOISE,
                 a1_noise=simulator.A1_NOISE,
                 bits=simulator.ADC_BITS,
-                all_positive=True
+                all_positive=True,
+                p0=[1, 0, 0, 0, 0, 0]
             )
 
             error_rad = simulator.rmse(phi, [e.value for e in n_res])
@@ -56,8 +57,7 @@ def run(phi, folder, samples=5, step=1, reps=10, max_cycles=8, show=False):
 
             method_errors.append(error_degrees)
 
-            time = simulator.total_time(cycles) / 60
-            logger.info(TPL_LOG.format(cycles, time, error_degrees_sci))
+            logger.info(TPL_LOG.format(cycles, error_degrees_sci))
 
         errors[method] = method_errors
 
@@ -68,10 +68,10 @@ def run(phi, folder, samples=5, step=1, reps=10, max_cycles=8, show=False):
         plot.add_data(cycles_list, errors[method], style=ms, ls=ls, color='k', lw=2, label=method)
 
     annotation = TPL_LABEL.format(samples, step)
-    plot._ax.text(0.05, 0.5, annotation, transform=plot._ax.transAxes)
+    plot._ax.text(0.05, 0.46, annotation, transform=plot._ax.transAxes)
 
     plot._ax.set_yscale('log')
-    plot.legend(fontsize=12)
+    plot.legend(loc='center right', fontsize=12)
 
     plot.save(filename=TPL_FILENAME.format(reps, samples, step))
 
