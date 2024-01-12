f8cf489a908027b9e1e32f57c5cb7ebe6d4eeb78
Tomás Link <tomaslink@gmail.com>
2024-01-12 15:12:16 -0300

    Add different ADC ranges in dynamic range simulation

--- rapp/simulations/error_vs_range.py
+++ rapp/simulations/error_vs_range.py
@@ -5,6 +5,8 @@ import numpy as np
 from rapp import constants as ct
 from rapp.simulations import simulator
 from rapp.signal.plot import Plot
+from rapp.adc import GAINS
+
 
 logger = logging.getLogger(__name__)
 
@@ -19,26 +21,27 @@ def run(phi, folder, method, samples=5, step=0.01, reps=1, cycles=2, show=False)
 
     xs = np.arange(0.1, 2, step=0.2)
 
-    errors = []
-    for amplitude in xs:
-        fc = simulator.samples_per_cycle(step=step)
+    errors = {}
+    for max_v, _ in list(GAINS.values())[::2]:
+        logger.info("MAX V={}, step={}".format(max_v, step))
+        errors[max_v] = []
+        for amplitude in xs:
+            fc = simulator.samples_per_cycle(step=step)
 
-        n_results = simulator.n_simulations(
-            phi=phi,
-            N=reps,
-            A=amplitude,
-            method='ODR',
-            cycles=cycles,
-            fc=fc,
-            fa=samples,
-        )
+            n_results = simulator.n_simulations(
+                phi=phi,
+                N=reps,
+                A=amplitude,
+                method='ODR',
+                cycles=cycles,
+                fc=fc,
+                fa=samples,
+            )
 
-        error = n_results.rmse()
-        errors.append(error)
+            error = n_results.rmse()
+            errors[max_v].append(error)
 
-        logger.info(TPL_LOG.format(amplitude, "{:.2E}".format(error)))
-
-    label = TPL_LABEL.format(cycles, samples, step, reps)
+            logger.info(TPL_LOG.format(amplitude, "{:.2E}".format(error)))
 
     percentages = ((xs * 2) / simulator.ADC_MAXV) * 100
 
@@ -50,11 +53,16 @@ def run(phi, folder, method, samples=5, step=0.01, reps=1, cycles=2, show=False)
         folder=folder
     )
 
-    plot.add_data(percentages, errors, color='k', style='s-', lw=1.5, label=label)
+    for max_v, errors in errors.items():
+        label = "max_v={} V".format(max_v)
+        plot.add_data(percentages, errors, style='.-', lw=2, label=label)
 
     plot._ax.set_xticks(plot._ax.get_xticks())
     plot._ax.set_yscale('log')
     plot.legend(fontsize=12)
+
+    annotation = TPL_LABEL.format(cycles, samples, step, reps)
+    plot._ax.text(0.046, 0.1, annotation, transform=plot._ax.transAxes)
 
     plot.save(filename=TPL_FILENAME.format(reps, samples, step))
 
