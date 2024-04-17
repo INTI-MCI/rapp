0dca2afd0b181277cacf07842e6a57e0b41ccc6b
LA <ldalvarez@inti.gob.ar>
2024-04-17 12:34:33 -0300

    txt filter in phase_diff

--- rapp/analysis/phase_diff.py
+++ rapp/analysis/phase_diff.py
@@ -25,7 +25,7 @@ def phase_difference_from_folder(folder, method, show=False, appended_measuremen
     logger.info("Calculating phase difference for {}...".format(folder))
 
     files = sorted([os.path.join(folder, x) for x in os.listdir(folder) if x.endswith('txt')])
-    # files = files[1:]  # First measurement is broken in data/2024-03-05-repeatability/hwp0
+    files = files[1:]  # First measurement is broken in data/2024-03-05-repeatability/hwp0
     # files = files[:-1]   # Last measurement is broken in data/2024-04-05-simple-setup
 
     results = []
