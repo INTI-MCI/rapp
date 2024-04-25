7688c41c4e6674263bbea496e7d78572b960fa56
PC-Latof-Raices <adminx@ptmwsf2151.inti.local>
2024-04-25 10:25:23 -0300

    add measurements simple setup with detector from obj channel add "HOME found" message in rotary_stage.py and last experiment config in experiment.py

--- examples/experiment.py
+++ examples/experiment.py
@@ -21,19 +21,19 @@ def setup_logger():
 
 def main():
     setup_logger()
-    e1 = dict(cycles=1, step=1, samples=169, reps=100, no_ch1=True)
-    e3 = dict(cycles=1, step=1, samples=1014, reps=100, no_ch1=True)
-    # e2 = dict(cycles=1, step=1, samples=169, reps=100, delay_position=1)
-    experimentos = [e3, e1]
+    e1 = dict(cycles=1, step=1, samples=169, reps=50, no_ch0=True)
+    e2 = dict(cycles=1, step=1, samples=169, reps=50, delay_position=1, no_ch0=True)
+    e3 = dict(cycles=1, step=1, samples=1014, reps=50, no_ch0=True)
+    experimentos = [e1, e2, e3]
 
     for i, exp in enumerate(experimentos, 1):
         logger.info("EXPERIMENTO {}: {}".format(i, exp))
         try:
-            main_polarimeter(prefix='min-setup2-{}'.format(i), **exp)
+            main_polarimeter(prefix='min-setup-detector-obj-{}'.format(i), **exp)
         except ESP301Error as e:
             logger.warning(f"Found error: {e}. Retrying...")
             time.sleep(30)
-            main_polarimeter(prefix='min-setup2-{}'.format(i), **exp)
+            main_polarimeter(prefix='min-setup-detector-obj-{}'.format(i), **exp)
 
 
 if __name__ == '__main__':
