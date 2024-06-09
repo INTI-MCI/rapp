a66e61a365c8ad77769b37173c8209caeecb7b31
Tomás Link <tomaslink@gmail.com>
2024-06-09 16:53:58 -0300

    Refactor and simplify CLI. Write measurements inside folder.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,7 +5,11 @@ from rapp import cli
 
 def test_polarimeter(monkeypatch, tmp_path):
     cli.run([
-        'polarimeter', '--work-dir', str(tmp_path), '--mock-esp', '--mock-adc', '--hwp-delay', '0'
+        'polarimeter',
+        '--work-dir', str(tmp_path),
+        '--mock-esp',
+        '--mock-adc',
+        '--hwp-delay-position', '0'
     ])
 
     with pytest.raises(SystemExit):
