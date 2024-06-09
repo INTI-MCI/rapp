6347189e2256e5332198f8e781bb5525085a6263
Tomás Link <tomaslink@gmail.com>
2024-06-09 16:53:58 -0300

    Refactor and simplify CLI. Write measurements inside folder.

--- rapp/cli.py
+++ rapp/cli.py
@@ -1,6 +1,9 @@
 import sys
+import os
 import logging
 import argparse
+
+from pathlib import Path
 
 from rich.logging import RichHandler
 
@@ -10,71 +13,89 @@ from rapp.commands import COMMANDS
 logger = logging.getLogger(__name__)
 
 
-APP_NAME = 'RAPP'
-DESCRIPTION = 'Tools for measuring the rotation angle of the plane of polarization (RAPP).'
-LOG_FORMAT = '%(name)s - %(message)s'
+class CLI:
+    """Generic command-line interface."""
+    HELP_VERBOSE = 'whether to run with DEBUG log level (default: %(default)s).'
 
+    def __init__(self, args):
+        self._args = args
 
-def setup_logger():
-    logging.basicConfig(
-        level=logging.INFO, format=LOG_FORMAT, handlers=[RichHandler(level="NOTSET")])
+        self._add_parser()
+        self._add_subparsers()
+        self._parse_args()
+        self._setup_logger()
 
-    # Hide logging from external libraries
-    external_libs = ['matplotlib', 'PIL']
-    for lib in external_libs:
-        logging.getLogger(lib).setLevel(logging.ERROR)
+    def execute(self):
+        config = vars(self.args)
+
+        return self.command.run(**config)
+
+    def _add_parser(self):
+        self.parser = argparse.ArgumentParser(
+            prog=self.NAME,
+            description=self.DESCRIPTION,
+            formatter_class=self.formatter())
+
+        self.parser.add_argument('-v', '--verbose', action='store_true', help=self.HELP_VERBOSE)
+
+    def _add_subparsers(self):
+
+        self.subparsers = self.parser.add_subparsers(dest='command', help='available commands')
+
+        for command in self.COMMANDS.values():
+            command.add_to_subparsers(self.subparsers)
+
+    def _parse_args(self):
+        self.args, self.extra_args = self.parser.parse_known_args(args=self._args or ['--help'])
+
+        if self.args.verbose:
+            logging.getLogger().setLevel(logging.DEBUG)
+
+        self.command = self.COMMANDS[self.args.command]
+
+        del self.args.verbose
+        del self.args.command
+
+    def _setup_logger(self):
+        rh = RichHandler(level="NOTSET")
+        rh.setFormatter(logging.Formatter(self.LOG_FORMAT_RICH))
+
+        os.makedirs(self.args.work_dir, exist_ok=True)
+        log_file = Path(self.args.work_dir).joinpath("{}.log".format(self.NAME))
+
+        logging.basicConfig(
+            level=logging.INFO,
+            format=self.LOG_FORMAT, handlers=[logging.FileHandler(log_file), rh])
+
+        for module in self.SUPRESS_LOG:
+            logging.getLogger(module).setLevel(logging.ERROR)
 
 
 class RAPPError(Exception):
     pass
 
 
-class RAPP:
-    def __init__(self, commands):
-        self.parser = argparse.ArgumentParser(prog=APP_NAME, description=DESCRIPTION)
-        self.subparsers = self.parser.add_subparsers(dest='command', help='available commands')
+class RAPP(CLI):
+    NAME = 'RAPP'
+    DESCRIPTION = 'Tools for measuring the rotation angle of the plane of polarization (RAPP).'
+    LOG_FORMAT_RICH = '%(name)s - %(message)s'
+    LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
-        self.commands = commands
+    # packages / moudules for which to supress any log level except ERROR.
+    SUPRESS_LOG = ['matplotlib', 'PIL']
 
-        for command in self.commands.values():
-            command.add_to_subparsers(self.subparsers)
+    COMMANDS = COMMANDS
 
-    def parse_args(self, args):
-        self.args = self.parser.parse_args(args=args or ['--help'])
+    @staticmethod
+    def formatter():
+        def formatter(prog):
+            return argparse.RawTextHelpFormatter(prog, max_help_position=50)
 
-        if self.args.command not in self.commands:
-            raise RAPPError("Invalid RAPP command: {}".format(self.command))
-
-        self.command = self.commands[self.args.command]
-
-        if self.args.verbose:
-            logging.getLogger().setLevel(logging.DEBUG)
-
-    def execute(self):
-        return self.command.run(**self._get_command_args())
-
-    def _get_command_args(self):
-        command_args = vars(self.args)
-        del command_args['command']
-        del command_args['verbose']
-
-        return command_args
+        return formatter
 
 
 def run(args):
-    setup_logger()
-
-    try:
-        logger.info("Initializing RAPP")
-        rapp = RAPP(COMMANDS)
-
-        logger.info("Parsing RAPP arguments...")
-        rapp.parse_args(args)
-
-        logger.info("Running RAPP command: {}".format(rapp.args.command))
-        rapp.execute()
-    except RAPPError as e:
-        rapp.logger.error(e)
+    RAPP(args).execute()
 
 
 def main():
