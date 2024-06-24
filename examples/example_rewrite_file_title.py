61f949e216e3717c27c17beead91dba95f9ad210
Camila Luciana Vargas <cvargas@inti.gob.ar>
2024-06-24 16:29:37 -0300

    Fix pep-8 error in example file

--- examples/example_rewrite_file_title.py
+++ examples/example_rewrite_file_title.py
@@ -14,20 +14,6 @@ def read_measurement_file(filepath, sep=r"\s+"):
     )
 
 
-folder = 'data/2024-06-12-full-quartz-2-cycles3-step1-samples169/hwp0-rep1.csv'
-folder = 'data/2024-06-12-full-quartz-1-cycles1-step1-samples169/hwp0-rep1.csv'
-
-# filename = os.path.join(folder, '/hwp0-rep1.csv')
-
-# data = read_measurement_file(folder)
-#
-# files = glob.glob(filename, recursive=True)
-# print(files)
-#
-# for file in files:
-#     data = read_measurement_file(file)
-#     print(data)
-
 title = 'ANGLE,CH0,CH1,NORM'
 folder = r'C:\Users\Admin\rapp\data\2024-06-16-quartz-velocities-5-cycles1-step1-samples169'
 pattern = os.path.join(folder, r"*.csv")
@@ -43,7 +29,7 @@ for file in files:
             lines_csv.append(linea)
         # lines = fid.readlines()
     lines_csv[3] = title.split(',')
-        # fid.writelines(lines)
+    # fid.writelines(lines)
     with open(file, 'w', newline='') as fid:
         csv_writer = csv.writer(fid)
         csv_writer.writerows(lines_csv)
