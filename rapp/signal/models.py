75d5b4d4dde47a6cfb6778b65b1151c84059a4e2
Camila Luciana Vargas <cvargas@inti.gob.ar>
2024-09-26 20:46:22 -0300

    Change model names so that they are more accurate, change way in which parameters are chosen in model with different amount of harmonics

--- rapp/signal/models.py
+++ rapp/signal/models.py
@@ -69,10 +69,14 @@ def two_diff_distorted_sines(x12, A1s, A2s, phi1s, deltas, C1, C2):
     return np.hstack([s1, s2])
 
 
-def two_sines_with_harmonics(x, *p):
-    n_harmonics = int((len(p) - 2) / 4)
-    parts = [p[i*n_harmonics:(i+1)*n_harmonics] for i in range(4)]
-    A1s, A2s, phi1s, deltas = parts
+def two_sines_with_harmonics(x, n_harmonics_ch0, n_harmonics_ch1, *p):
+    n_ch0 = int(n_harmonics_ch0)
+    n_ch1 = int(n_harmonics_ch1)
+    n = n_ch0 + n_ch1
+    A1s = p[0:n_ch0]
+    A2s = p[n:n+n_ch1]
+    phi1s = p[2*n:(2*n)+n_ch0]
+    deltas = p[3*n:(3*n)+n_ch1]
     C1, C2 = p[-2:]
     return two_diff_distorted_sines(x, A1s, A2s, phi1s, deltas, C1, C2)
 
