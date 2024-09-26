872594c5190c9f329e0135ce32033659946213f7
Camila Luciana Vargas <cvargas@inti.gob.ar>
2024-09-26 15:39:33 -0300

    Change model with different amount of harmonics in each channel

--- rapp/signal/models.py
+++ rapp/signal/models.py
@@ -40,7 +40,7 @@ def two_distorted_sines(x12, A1s, A2s, phi1s, deltas, C1, C2):
     return np.hstack([s1, s2])
 
 
-def two_sines_with_harmonics(x, *p):
+def two_sines_same_harmonics(x, *p):
     n_harmonics = int((len(p) - 2) / 4)
     parts = [p[i*n_harmonics:(i+1)*n_harmonics] for i in range(4)]
     A1s, A2s, phi1s, deltas = parts
@@ -48,33 +48,33 @@ def two_sines_with_harmonics(x, *p):
     return two_distorted_sines(x, A1s, A2s, phi1s, deltas, C1, C2)
 
 
-def one_distorted_sine(x12, A1s, A2s, phi1s, deltas, C1, C2):
+def two_diff_distorted_sines(x12, A1s, A2s, phi1s, deltas, C1, C2):
     total = len(x12)
     half = total // 2
 
     x1 = x12[0:half]
     x2 = x12[half:total]
 
-    A2 = A2s[0]
-    phi1 = phi1s[0]
-    delta = deltas[0]
-
-    s1 = np.zeros_like(x1)
-    for h, (A1, A2, phi1, delta) in enumerate(zip(A1s, A2s, phi1s, deltas)):
+    s1, s2 = np.zeros_like(x1), np.zeros_like(x2)
+    for h, (A1, phi1) in enumerate(zip(A1s, phi1s)):
         s1 = s1 + sine((h + 1) * x1, A1, phi1, 0)
     s1 = s1 + C1
 
-    s2 = sine(x2, A2, phi1 + delta, C2)
+    np.resize(phi1s, len(deltas))
+
+    for h, (A2, phi1, delta) in enumerate(zip(A2s, phi1s, deltas)):
+        s2 = s2 + sine((h + 1) * x2, A2, phi1 + delta, 0)
+    s2 = s2 + C2
 
     return np.hstack([s1, s2])
 
 
-def one_sine_with_harmonic(x, *p):
+def two_sines_with_harmonics(x, *p):
     n_harmonics = int((len(p) - 2) / 4)
     parts = [p[i*n_harmonics:(i+1)*n_harmonics] for i in range(4)]
     A1s, A2s, phi1s, deltas = parts
     C1, C2 = p[-2:]
-    return one_distorted_sine(x, A1s, A2s, phi1s, deltas, C1, C2)
+    return two_diff_distorted_sines(x, A1s, A2s, phi1s, deltas, C1, C2)
 
 
 def two_sines_with_harmonics_objective(p, *args):
