be3533ba4f95dc18bf65cb3d8e1fa4ce87d88ae6
Camila Luciana Vargas <cvargas@inti.gob.ar>
2024-11-06 14:48:22 -0300

    Add header in temperature measurement file

--- examples/example_temperature.py
+++ examples/example_temperature.py
@@ -3,24 +3,20 @@ import logging
 import struct
 import os
 from datetime import date
-
+import json
 import serial
-import numpy as np
-from pathlib import Path
 
-from rapp.mocks import SerialMock
-from rapp import constants as ct
 
 logger = logging.getLogger(__name__)
 
 PORT = 'COM3'
 BAUDRATE = 57600
-TIMEOUT = 0.1
+TIMEOUT = 1
 WAIT = 2
 CMD = "temp?\n"
-MEASUREMENT_WAIT = 8#60
-MEASUREMENT_TIME = 40#3600
-SAMPLES = 2
+MEASUREMENT_WAIT = 7#60
+MEASUREMENT_TIME = 30#3600
+SAMPLES = 10
 
 FILENAME = 'temperatura'
 
@@ -43,25 +39,37 @@ def get_serial_connection(*args, **kwargs):
 
 def main(port=PORT, baudrate=BAUDRATE, timeout=TIMEOUT, wait=WAIT, cmd=CMD,
          measurement_wait=MEASUREMENT_WAIT, measurement_time=MEASUREMENT_TIME, samples=SAMPLES):
+    assert measurement_wait < measurement_time, "measurement_wait must be less than measurement_time"
+    assert measurement_wait > samples * 0.5, "measurement_wait must be greater than samples * 0.5"
+
     print("Instantiating ADC...")
     serial_connection = get_serial_connection(port, baudrate=baudrate, timeout=timeout)
     logger.info("Waiting {} seconds after connecting to ADC...".format(wait))
     # Arduino resets when a new serial connection is made.
     # We need to wait, otherwise we don't receive anything.
     time.sleep(wait)
-
+    flag_header = True
     hora = time.time()
     while time.time() - hora < measurement_time:
-        hora_inicio = time.time()
         temperaturas = [0] * samples
+        hora_inicio = time.time()
         for i in range(samples):
             serial_connection.write(bytes(cmd, 'utf-8'))
-            time.sleep(0.5)
-            temp = serial_connection.readline()
+            temp = serial_connection.read(4)
             temp = struct.unpack('<f', temp)[0]
-            temperaturas[i] = '{}\n'.format(temp)
-        with open(r"{measurement_dir}".format(measurement_dir=measurement_dir), 'a') as f:
-            f.writelines(temperaturas)
+            temperaturas[i] = '{}'.format(temp)
+        if flag_header:
+            hora_fin = time.time()
+            tiempo_mediciones = hora_fin - hora_inicio
+            header = {'tiempo_total': measurement_time, 'tiempo_espera': measurement_wait, 'muestras': samples, 'tiempo_mediciones': tiempo_mediciones}
+            header = json.dumps(header)
+            with open(measurement_dir, 'w') as f:
+                f.write(header + '\n')
+            flag_header = False
+
+        with open(measurement_dir, 'a') as f:
+            f.writelines(",".join(temperaturas) + '\n')
+        print(temperaturas)
 
         hora_fin = time.time()
         tiempo_mediciones = hora_fin - hora_inicio
